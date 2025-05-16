06b86679f0ed6843d1197770c59032f8c8325db4
rinshadibnabu <rinshadpibnabu@gmail.com>
2025-05-16 06:30:11 +0530

    add seed script

---
+++ packages/db/prisma/seed.ts
@@ -0,0 +1,55 @@
+import { prisma } from "../src/client"
+import bcrypt from "bcrypt"
+async function main() {
+
+  let hashedPassword1 = await bcrypt.hash("alice", 10);
+  let hashedPassword2 = await bcrypt.hash('bob', 10);
+  const alice = await prisma.user.upsert({
+    where: { number: '9999999999' },
+    update: {},
+    create: {
+      number: '9999999999',
+      password: hashedPassword1,
+      name: 'alice',
+
+      OnRampTransaction: {
+        create: {
+          startTime: new Date(),
+          status: "Success",
+          amount: 20000,
+          token: "122",
+          provider: "HDFC Bank",
+        },
+      },
+
+    },
+  })
+  const bob = await prisma.user.upsert({
+    where: { number: '9999999998' },
+    update: {},
+    create: {
+      number: '9999999998',
+      password: hashedPassword2,
+      name: 'bob',
+      OnRampTransaction: {
+        create: {
+          startTime: new Date(),
+          status: "Failure",
+          amount: 2000,
+          token: "123",
+          provider: "HDFC Bank",
+        },
+      },
+    },
+  })
+  console.log({ alice, bob })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
