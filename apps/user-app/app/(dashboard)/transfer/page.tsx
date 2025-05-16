23da29f617e7a217338fff7d3e193878f5df7947
rinshadibnabu <rinshadpibnabu@gmail.com>
2025-05-16 06:26:00 +0530

    edit transfer page

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -46,7 +46,7 @@ export default async function () {
         <AddMoney />
       </div>
       <div>
-        <BalanceCard amount={balance.amount / 100} locked={balance.locked} />
+        <BalanceCard amount={balance.amount} locked={balance.locked} />
         <div className="pt-4">
           <OnRampTransactions transactions={transactions} />
         </div>
